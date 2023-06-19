f2d1b854b7493d0e986bda0577928344f8d40b5e
Sahin Vardar <sahin@vardar.io>
2023-06-19 17:24:56 +0200

    fix: handles ts script tags as well

--- assets/example/routes/+page.svelte
+++ assets/example/routes/+page.svelte
@@ -1,7 +1,11 @@
-<script>
+<script lang="ts">
+  function $i18n(key: string, params?: any) {
+    return key;
+  }
   let title = $i18n('title');
   const def = [$i18n('item0'), $i18n('item1')];
 </script>
+
 <main>
   <section>
     {$i18n('intro')}
